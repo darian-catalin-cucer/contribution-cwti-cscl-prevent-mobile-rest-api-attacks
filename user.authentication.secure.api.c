64302cacdc3b7018702afe4a16231c42e6a73bc3
Darian-Cătălin (Dorian) Cucer <cucerdariancatalin@gmail.com>
2023-04-19 09:18:49 +0300

    Create user.authentication.secure.api.c

---
+++ user.authentication.secure.api.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <curl/curl.h>
+
+int main(void)
+{
+  CURL *curl;
+  CURLcode res;
+
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "https://api.example.com");
+    curl_easy_setopt(curl, CURLOPT_USERNAME, "USERNAME");
+    curl_easy_setopt(curl, CURLOPT_PASSWORD, "PASSWORD");
+    res = curl_easy_perform(curl);
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+    curl_easy_cleanup(curl);
+  }
+  return 0;
+}
