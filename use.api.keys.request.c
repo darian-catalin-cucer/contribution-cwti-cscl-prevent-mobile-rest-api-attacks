4bfb4fd622fd9889f22bcd473f2a8347ae75d05d
Darian-Cătălin (Dorian) Cucer <cucerdariancatalin@gmail.com>
2023-04-19 09:18:28 +0300

    Create use.api.keys.request.c

---
+++ use.api.keys.request.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <curl/curl.h>
+
+int main(void)
+{
+  CURL *curl;
+  CURLcode res;
+
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "https://api.example.com?key=APIKEY");
+    res = curl_easy_perform(curl);
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+    curl_easy_cleanup(curl);
+  }
+  return 0;
+}
